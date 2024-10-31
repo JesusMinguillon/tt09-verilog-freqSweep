ed72403a36175b0e5cd8f0e2faf54952005935d8
JesusMinguillon <jesus.minguillon.campos@gmail.com>
2024-10-31 09:35:59 +0100

    update test files

--- test/tb.v
+++ test/tb.v
@@ -22,28 +22,38 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
+  `ifdef GL_TEST
+    wire VPWR = 1'b1;
+    wire VGND = 1'b0;
+  `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_JesusMinguillon_freqSweep user_project (
 
       // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
+      `ifdef GL_TEST
+        .VPWR(VPWR),
+        .VGND(VGND),
+      `endif
 
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
       .uio_out(uio_out),  // IOs: Output path
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+      .ena    (ena),      // Enable - goes high when design is selected
+      .clk    (clk),      // Clock
+      .rst_n  (rst_n)     // Not reset
+  );
+
+  wire [4:0] clk_factor;
+
+  periodCount pc1 (
+      .clk1       (clk),
+      .clk2       (uo_out[0]),
+      .ena        (ui_in[0]),
+      .rst_n      (rst_n),
+      .clk_factor (clk_factor)
   );
 
 endmodule
--- test/test.py
+++ test/test.py
@@ -4,37 +4,148 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
-
+from cocotb.triggers import Timer
+from cocotb.triggers import RisingEdge
 
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
+async def test_startup(dut):
+    dut._log.info("Startup test with reset and internal enable")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    # Set the clock period to 200 ns (5 MHz)
+    clock = Clock(dut.clk, 200, units="ns")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
+    # Test
+    dut._log.info("rst_n = 0, ui_in[0] = 0")
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 1)
+    await Timer(2, units='ns')
+    assert dut.uo_out[0].value == 0
+    dut._log.info("uo_out[0] = 0")
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
+    dut._log.info("rst_n = 1, ui_in[0] = 0")
+    await RisingEdge(dut.clk)
+    await Timer(2, units='ns')
+    assert dut.uo_out[0].value == 0
+    dut._log.info("uo_out[0] = 0")
+    await ClockCycles(dut.clk, 9)
+    dut.ui_in[0].value = 1
+    dut._log.info("rst_n = 1, ui_in[0] = 1")
+    await RisingEdge(dut.clk)
+    await Timer(2, units='ns')
+    assert dut.uo_out[0].value == 1
+    dut._log.info("uo_out[0] = 1")
+    await RisingEdge(dut.clk)
+    await Timer(2, units='ns')
+    assert dut.uo_out[0].value == 0
+    dut._log.info("uo_out[0] = 0")
+    dut._log.info("Wait until 6 us")
+    await ClockCycles(dut.clk, 8)
+    dut._log.info("End of startup test")
 
-    dut._log.info("Test project behavior")
+@cocotb.test()
+async def test_reset(dut):
+    dut._log.info("Reset test")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # Set the clock period to 200 ns (5 MHz)
+    clock = Clock(dut.clk, 200, units="ns")
+    cocotb.start_soon(clock.start())
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    # Test
+    dut._log.info("rst_n = 0, ui_in[0] = 1")
+    dut.rst_n.value = 0
+    await Timer(2, units='ns')
+    assert dut.uo_out[0].value == 0
+    dut._log.info("uo_out[0] = 0")
+    assert dut.clk_factor.value == 1
+    dut._log.info("clk_factor = 1")
+    await RisingEdge(dut.clk)
+    await Timer(2, units='ns')
+    assert dut.uo_out[0].value == 0
+    dut._log.info("uo_out[0] = 0")
+    assert dut.clk_factor.value == 1
+    dut._log.info("clk_factor = 1")
+    await ClockCycles(dut.clk, 9)
+    assert dut.uo_out[0].value == 0
+    dut._log.info("uo_out[0] = 0")
+    assert dut.clk_factor.value == 1
+    dut._log.info("clk_factor = 1")
+    dut.rst_n.value = 1
+    dut._log.info("rst_n = 1, ui_in[0] = 1")
+    await RisingEdge(dut.clk)
+    await Timer(2, units='ns')
+    assert dut.uo_out[0].value == 1
+    dut._log.info("uo_out[0] = 1")
+    assert dut.clk_factor.value == 1
+    dut._log.info("clk_factor = 1")
+    await RisingEdge(dut.clk)
+    await Timer(2, units='ns')
+    assert dut.uo_out[0].value == 0
+    dut._log.info("uo_out[0] = 0")
+    assert dut.clk_factor.value == 1
+    dut._log.info("clk_factor = 1")
+    dut._log.info("Wait until 10 us")
+    await ClockCycles(dut.clk, 8)
+    dut._log.info("End of reset test")
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+@cocotb.test()
+async def test_internal_enable(dut):
+    dut._log.info("Internal enable test")
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # Set the clock period to 200 ns (5 MHz)
+    clock = Clock(dut.clk, 200, units="ns")
+    cocotb.start_soon(clock.start())
+
+    # Test
+    dut._log.info("rst_n = 1, ui_in[0] = 0")
+    dut.ui_in[0].value = 0
+    await ClockCycles(dut.clk, 8)
+    dut.ui_in[0].value = 1
+    dut._log.info("rst_n = 1, ui_in[0] = 1")
+    await RisingEdge(dut.clk)
+    await Timer(2, units='ns')
+    assert dut.uo_out[0].value == 1
+    dut._log.info("uo_out[0] = 1")
+    assert dut.clk_factor.value == 2
+    dut._log.info("clk_factor = 2")
+    dut._log.info("Wait until 13 us")
+    await ClockCycles(dut.clk, 6)
+    dut._log.info("Reset for 800 ns and wait 200 ns")
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 4)
+    dut.rst_n.value = 1
+    await RisingEdge(dut.clk)
+    dut._log.info("End of internal enable test")
+
+@cocotb.test()
+async def test_period_count(dut):
+    dut._log.info("Period count test")
+
+    # Set the clock period to 200 ns (5 MHz)
+    clock = Clock(dut.clk, 200, units="ns")
+    cocotb.start_soon(clock.start())
+
+    # Test
+    await ClockCycles(dut.clk, 2)
+    await Timer(2, units='ns')
+    assert dut.clk_factor.value == 2
+    dut._log.info("clk_factor = 2")
+    await ClockCycles(dut.clk, 32)
+    await Timer(2, units='ns')
+    assert dut.clk_factor.value == 4
+    dut._log.info("clk_factor = 4")
+    await ClockCycles(dut.clk, 64)
+    await Timer(2, units='ns')
+    assert dut.clk_factor.value == 8
+    dut._log.info("clk_factor = 8")
+    await ClockCycles(dut.clk, 128)
+    await Timer(2, units='ns')
+    assert dut.clk_factor.value == 16
+    dut._log.info("clk_factor = 16")
+    dut._log.info("Wait until 200 us")
+    await ClockCycles(dut.clk, 704)
+    dut._log.info("End of period count test")
